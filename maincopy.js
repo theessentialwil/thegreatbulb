bae56a161b1ea96ad4c81b34b7b55b6e9fa850c5
mukulembezewil <wilmukulembeze@gmail.com>
2022-11-08 11:58:37 +0300

    bulb firs sprint

---
+++ maincopy.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data() {
+      return {
+          status: true,
+          message: "Testing this"
+      }
+  },
+  // methods: {
+  //   // switchOnOff() {
+  //   //     this.status = !this.status
+  //   // }
+  // }
+     
+})
